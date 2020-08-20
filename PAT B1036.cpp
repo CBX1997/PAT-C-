f5236fa5a266822310dc1d19d2de575532358439
IC-Boxuan <1159834330@qq.com>
2020-08-20 20:33:31 +0800

    PAT B1036 跟着奥巴马学编程

--- PAT B1036.cpp
+++ PAT B1036.cpp
@@ -1,8 +1,8 @@
 #include<cstdio>
 int main() {
-	int row,col,n;
+	int row, col, n;
 	char m;
-	scanf_s("%d %c", &col,&m);
+	scanf_s("%d %c", &col, &m);
 	if (col % 2 == 0)
 		row = col / 2;
 	else
@@ -11,16 +11,16 @@ int main() {
 	for (int i = 0; i < col; i++) {
 		printf("%c", m);
 	}
-	for (int i = 0; i <row; i++)
+	for (int i = 0; i < row; i++)
+	{
+		printf("\n");
+		printf("%c", m);
+		for (int n = 0; n < (col - 2); n++)
 		{
-			printf("\n");
-			printf("%c", m);
-			for (int n = 0; n <(col - 2); n++) 
-			{
-				printf(" ");
-			}
-			printf("%c", m);
+			printf(" ");
 		}
+		printf("%c", m);
+	}
 	printf("\n");
 	for (int i = 0; i < col; i++)
 		printf("%c", m);
